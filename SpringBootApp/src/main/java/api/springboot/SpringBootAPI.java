27b2d443039b85b8432597f6674fe8dc8e2c07dc
VipulKumarSharma <tysonvks@gmail.com>
2018-02-18 21:50:07 +0530

    Spring Boot API
    Spring Boot API Commit

---
+++ SpringBootApp/src/main/java/api/springboot/SpringBootAPI.java
@@ -0,0 +1,13 @@
+package api.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootAPI {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootAPI.class, args);
+	}
+
+}
