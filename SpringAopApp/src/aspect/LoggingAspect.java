396ba9a4ee36aa8f8fec2538e7cced85cc0a9d01
tysonvks <tysonvks@tysonvks-linux>
2018-05-06 21:53:04 +0530

    Combining Pointcuts

--- SpringAopApp/src/aspect/LoggingAspect.java
+++ SpringAopApp/src/aspect/LoggingAspect.java
@@ -6,27 +6,50 @@ import org.aspectj.lang.annotation.Pointcut;
 
 @Aspect
 public class LoggingAspect {
-	public static final String pointcutExpression = "execution(public * getN*())";
 	
-	/* execution(public String getName()
-	 * execution(public String model.Circle.getName()
-	 * execution(public * get*())"
-	 * execution(* get*(*))" 					// Any type of argument
-	 * execution(* get*(..))" 					// Any no. and any type of argument
-	 * execution(* model.*.get*(..))"
+	/*  POINTCUT EXPRESSIONS - takes parameter as Method(s)
+	 
+	 * 	execution(public String getName()
+	 * 	execution(public String model.Circle.getName()
+	 * 	execution(public * get*())"
+	 * 	execution(* get*(*))" 					// Any type of argument
+	 * 	execution(* get*(..))" 					// Any no. and any type of argument
+	 * 	execution(* model.*.get*(..))"
 	 */
 	
+	public static final String pointcutExpression 
+	= "execution(* model.Circle.*(..))";
 	
 	@Before("execution(public * getN*())")
 	public void LoggingAdvice() {
 		System.out.println("LoggingAdvice() : Get method called");
 	}
 	
+	
+	
+	/*  WITHIN EXPRESSIONS - takes parameter as Class(s)
+	 
+	 * 	within(model.Circle)
+	 * 	within(model.*)
+	 */
+	
+	@Pointcut(LoggingAspect.pointcutExpression)
+	public void allGetters() {}					// Dummy Method that holds pointcut expression
+	
+	public static final String pointcutExpression_within 
+	= "within(model.Circle)";
+	
+	@Pointcut(LoggingAspect.pointcutExpression_within)
+	public void allCircles() {}					// Dummy Method that holds pointcut expression
+	
 	@Before("allGetters()")
 	public void LoggingAdvice_Pointcut() {
 		System.out.println("LoggingAdvice_Pointcut() : Get method called");
 	}
 	
-	@Pointcut(LoggingAspect.pointcutExpression)
-	public void allGetters() {}				// Dummy Method that holds pointcut expression
+	@Before("allGetters() && allCircles()")		// Combining Pointcuts
+	public void LoggingAdvice_Combining_Pointcuts() {
+		System.out.println("LoggingAdvice_Combining_Pointcuts() : method called");
+	}
+	
 }
