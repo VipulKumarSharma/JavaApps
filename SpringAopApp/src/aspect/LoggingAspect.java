df192ce0a9d7810b390c0082a76b898ac9df5f63
tysonvks <tysonvks@tysonvks-linux>
2018-05-06 21:33:24 +0530

    Pointcut Expressions

--- SpringAopApp/src/aspect/LoggingAspect.java
+++ SpringAopApp/src/aspect/LoggingAspect.java
@@ -2,17 +2,31 @@ package aspect;
 
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
 
 @Aspect
 public class LoggingAspect {
+	public static final String pointcutExpression = "execution(public * getN*())";
 	
-	// @Before("execution(public String getName())")
-	// @Before("execution(public String model.Circle.getName())")
-	// @Before("execution(public * get*())")
-	// @Before("execution(* get*(*))") // Any type of argument
-	// @Before("execution(* get*(..))") // Any no. and any type of argument
-	@Before("execution(* model.*.get*(..))")
+	/* execution(public String getName()
+	 * execution(public String model.Circle.getName()
+	 * execution(public * get*())"
+	 * execution(* get*(*))" 					// Any type of argument
+	 * execution(* get*(..))" 					// Any no. and any type of argument
+	 * execution(* model.*.get*(..))"
+	 */
+	
+	
+	@Before("execution(public * getN*())")
 	public void LoggingAdvice() {
-		System.out.println("Advice run, Get method called");
+		System.out.println("LoggingAdvice() : Get method called");
 	}
+	
+	@Before("allGetters()")
+	public void LoggingAdvice_Pointcut() {
+		System.out.println("LoggingAdvice_Pointcut() : Get method called");
+	}
+	
+	@Pointcut(LoggingAspect.pointcutExpression)
+	public void allGetters() {}				// Dummy Method that holds pointcut expression
 }
